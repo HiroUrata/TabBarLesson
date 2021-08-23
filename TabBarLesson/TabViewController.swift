fd32fbd6257091567178c9d7c530e22dbdfb9c19
HiroUrata <1712026@i.softbank.jp>
2021-08-23 21:56:42 +0900

    tabbar　アニメーション　追加

--- TabBarLesson/TabViewController.swift
+++ TabBarLesson/TabViewController.swift
@@ -7,19 +7,46 @@
 
 import UIKit
 
-class TabViewController: UITabBarController {
+class TabViewController: UITabBarController,UITabBarControllerDelegate {
 
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tabBarController?.delegate = self
+        
+        tabBar.barTintColor = .systemIndigo
+        tabBar.unselectedItemTintColor = .white
+        tabBar.tintColor = .systemGreen
+        
+        tabBar.layer.borderWidth = 1.0
+        tabBar.layer.borderColor = UIColor.systemGreen.cgColor
+        
+        tabBar.layer.cornerRadius = 45.0
+        tabBar.layer.masksToBounds = true
+        
         
         
     }
     
     
-    
-    
 
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+
+        if item.selectedImage == UIImage(systemName: "arrow.up.and.down.circle") {
+
+            UITabBar.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {self.tabBar.frame.origin.y = self.tabBar.frame.origin.y + self.tabBar.frame.size.height}, completion: nil)
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+
+                UITabBar.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {self.tabBar.frame.origin.y = self.tabBar.frame.origin.y - self.tabBar.frame.size.height}, completion: nil)
+
+            }
+
+        }
+
+      }
+        
+    
+    
 }
